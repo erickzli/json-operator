c91ed08f71000468968978e21878b059b88598d0
Erick Li <ericklizh@gmail.com>
2023-06-09 10:29:16 -0500

    Add JSON sorting function

--- json-operator.py
+++ json-operator.py
@@ -3,6 +3,16 @@ import yaml
 import logging, sys
 
 
+def json_sorter(data, hier, sortkey, reverse=False):
+    """Sort JSON object based on sortkey provided the hierarchy of the
+    JSON object structure.
+    """
+    for level in hier:
+        data = data.get(level)
+    
+    return sorted(data, key=lambda d: d[sortkey], reverse=reverse)
+
+
 # Open the config YAML file
 with open('config.yml', 'r') as yml_file:
     config_dict = yaml.safe_load(yml_file)
@@ -22,6 +32,9 @@ json_file = open(input_filename)
 
 # Return the JSON object as a dictionary
 json_data = json.load(json_file)
+logging.debug(json_data.get('nodes')[0])
+sorted_node_list = json_sorter(json_data, ['nodes'], 'label')
+logging.debug(sorted_node_list[0])
 
 # Close the JSON file
 json_file.close()
